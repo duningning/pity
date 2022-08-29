962c87978926855953fe4f9ea669c14335c80439
duningning <duning132@163.com>
2022-08-29 15:04:32 +0800

    run.py测试日志demo 创建logs目录

--- run.py
+++ run.py
@@ -4,10 +4,13 @@
 # 文件   : run.py.py
 # IDE   : PyCharm
 from app import pity
+from app.utils import Log
 
 
 @pity.route('/')
 def hello_world():
+    log = Log('hello world专用')
+    log.info('有人访问你的网站了')
     return 'Hello World!'
 
 
