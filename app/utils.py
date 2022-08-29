91fed9e2e6b1794da360dca9d7cd45099e19bf22
duningning <duning132@163.com>
2022-08-29 14:59:21 +0800

    添加并编写日志类

---
+++ app/utils.py
@@ -0,0 +1,33 @@
+# -*-coding:utf-8-*-
+# 作者:duningning.ming
+# 创建时间: 2022/8/29 14:57
+# 文件   : utils.py
+# IDE   : PyCharm
+import logbook
+
+from app import pity
+
+
+class Log(object):
+    handler = None
+
+    def __init__(self, name='pity', filename=pity.config['LOG_NAME']):  # Logger标识默认为app
+        """
+        :param name: 业务名称
+        :param filename: 文件名称
+        """
+        self.handler = logbook.FileHandler(filename, encoding='utf-8')
+        self.logger = logbook.Logger(name)
+        self.handler.push_application()
+
+    def info(self, *args, **kwargs):
+        return self.logger.info(*args, **kwargs)
+
+    def error(self, *args, **kwargs):
+        return self.logger.error(*args, **kwargs)
+
+    def warning(self, *args, **kwargs):
+        return self.logger.warning(*args, **kwargs)
+
+    def debug(self, *args, **kwargs):
+        return self.logger.debug(*args, **kwargs)
