2c04143232d2a90d298278c3a58e43799ebea206
duningning <duning132@163.com>
2022-08-29 17:33:01 +0800

    使用Flask蓝图

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,14 @@
 # 文件   : __init__.py
 # IDE   : PyCharm
 from flask import Flask
+
+from app.controller.auth.user import auth
 from config import Config
 
 pity = Flask(__name__)
+
+
+# 注册蓝图
+pity.register_blueprint(auth)
+
 pity.config.from_object(Config)
