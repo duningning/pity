1119b2f8d42f1ff37391d6fc48d0d1c0523e87e5
duningning <duning132@163.com>
2022-09-15 11:53:19 +0800

    编写pity/handler/factory.py，目的是为了转换orm对象为dict

---
+++ app/handler/factory.py
@@ -0,0 +1,26 @@
+# -*-coding:utf-8-*-
+# 作者:duningning.ming
+# 创建时间: 2022/9/15 11:24
+# 文件   : factory.py
+# IDE   : PyCharm
+from datetime import datetime
+
+class ResponseFactory(object):
+
+    @staticmethod
+    def model_to_dict(obj, *ignore: str):
+        data = dict()
+        for c in obj.__table__.columns:
+            if c.name in ignore:
+                #如果字段忽略，则不进行转换
+                continue
+            val = getattr(obj, c.name)
+            if isinstance(val, datetime):
+                data[c.name] = val.strftime("%Y-%m-%d %H:%M:%S")
+            else:
+                data[c.name] = val
+        return data
+
+    @staticmethod
+    def model_to_list(data: list, *ignore: str):
+        return [ResponseFactory.model_to_dict(x, *ignore) for x in data]
